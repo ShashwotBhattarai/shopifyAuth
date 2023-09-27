a46fda72597770fc38dec1af18f5847dec870c0e
ShashwotBhattarai <shashwot.tech@gmail.com>
2023-09-27 15:54:53 +0545

    working until access token

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express= require('express');
 const app= express();
+const axios = require('axios');
 
 const SHOPIFY_API_KEY = "54f776fc8540393fb02b6b7cd8d1e7ed";
 const SHOPIFY_API_SECRET = "4019bcec519041faa1eb67efed82d95a";
@@ -14,29 +15,22 @@ app.listen(3000,()=>{
 app.get('/shopify/auth', (req, res) => {
     const shop = req.query.shop;
     console.log("inside auth");
-  
-    // Redirect the user to Shopify's authorization URL
     res.redirect(`https://${shop}/admin/oauth/authorize?client_id=${SHOPIFY_API_KEY}&scope=read_products&redirect_uri=${REDIRECT_URI}`);
   });
 
 
   app.get('/shopify/auth/redirect', async (req, res) => {
     const { code, shop } = req.query;
-  
-    // Exchange the authorization code for an access token
+    console.log("code"+code);
     const accessTokenResponse = await axios.post(`https://${shop}/admin/oauth/access_token`, {
       client_id: SHOPIFY_API_KEY,
       client_secret: SHOPIFY_API_SECRET,
       code,
     });
-  
-    // Store the access token securely (e.g., in a database)
+
     const accessToken = accessTokenResponse.data.access_token;
 
     console.log(accessToken);
-  
-    // Now you can use the accessToken to make API requests on behalf of the user
-    // ...
   
     res.send('OAuth successful! You can now make Shopify API requests.');
   });
